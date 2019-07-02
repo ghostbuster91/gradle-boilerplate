46dc3b8bcad9fa158035271b7e59c48b1b501f29
ghostbuster91 <kghost0@gmail.com>
2019-07-02 22:29:42 +0200

    [Refactor] Apply java8 style

--- src/main/java/io/ghostbuster91/boilerplate/GradleBoilerplate.java
+++ src/main/java/io/ghostbuster91/boilerplate/GradleBoilerplate.java
@@ -5,20 +5,13 @@ import spray.boilerplate.SbtBoilerplateRunner;
 
 import java.io.File;
 
+@SuppressWarnings("unused")
 public class GradleBoilerplate implements Plugin<Project> {
     public void apply(final Project project) {
-        project.getTasks().create("generateBoilerplate", DefaultTask.class, new Action<DefaultTask>() {
-            @Override
-            public void execute(DefaultTask defaultTask) {
+        project.getTasks().create("generateBoilerplate", DefaultTask.class, defaultTask -> {
 //                defaultTask.getInputs().dir(SbtBoilerplateRunner.resourcesDir(project)); //TODO: fix
-                defaultTask.getOutputs().dir(new File(new File(new File(project.getBuildDir(), "generated"), "src"), "java"));
-                defaultTask.doLast(new Action<Task>() {
-                    @Override
-                    public void execute(Task task) {
-                        SbtBoilerplateRunner.run(project);
-                    }
-                });
-            }
+            defaultTask.getOutputs().dir(new File(new File(new File(project.getBuildDir(), "generated"), "src"), "java"));
+            defaultTask.doLast(task -> SbtBoilerplateRunner.run(project));
         });
     }
 }
